71c548f5f0de62508c6488a54b03f4cc30923578
Serhii Vasylenko <github.com@vasylenko.info>
2022-05-11 13:22:16 +0200

    google analytics (#38)

--- server.sh
+++ server.sh
@@ -1,10 +1,30 @@
 #!/bin/zsh
 git submodule update --init --recursive
 git submodule update --remote --merge
-if [[ $1 == "localhost" ]]
-then
+
+bindip=$(ipconfig getifaddr en0)
+site_env=development
+
+for arg in "$@"
+do
+case $arg in
+    localhost)
     bindip=localhost
-else
-    bindip=$(ipconfig getifaddr en0)
-fi
-hugo server  --bind ${bindip} --baseURL http://${bindip} --buildDrafts --buildFuture --environment development --gc --noHTTPCache --quiet
+    shift
+    ;;
+    production)
+    site_env=production
+    shift
+    ;;
+    development)
+    site_env=development
+    shift
+    ;;
+    *)
+    # unknown option
+    echo -e "Unknown option $arg"
+    exit 2
+    ;;
+esac
+done
+hugo server  --bind ${bindip} --baseURL http://${bindip} --buildDrafts --buildFuture --environment $site_env --gc --noHTTPCache --quiet
